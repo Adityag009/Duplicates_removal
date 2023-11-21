9eb68a24ac259c1f839c662d308cbd233929301a
Adityag009 <adityaghadge99@gmail.com>
2023-11-21 17:27:18 +0530

    chnages in paid

--- paid_remover.py
+++ paid_remover.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import base64 
 
 def remove_rows_and_save(df_large, df_small, key_column):
     df_modified = df_large.merge(df_small[key_column], on=key_column, how='left', indicator=True)
@@ -14,14 +15,23 @@ st.title('Paid Remover Tool')
 file1 = st.file_uploader("Upload paid sheet CSV file", type=["csv"])
 file2 = st.file_uploader("Upload lead sheet CSV file", type=["csv"])
 # key_column = st.text_input("Enter the key column name for matching rows")
-key_column=['Email']
+key_column='Email'
 
 
 if st.button('Process Files'):
     if file1 is not None and file2 is not None and key_column:
         df1 = pd.read_csv(file1)
+            # Showing shape before removing duplicates
+        st.write('Shape of paid sheet', df1.shape)
         df2 = pd.read_csv(file2)
+
+        st.write('Shape of lead sheet', df2.shape)
+        # Convert column names to a uniform case (e.g., lower case) for both dataframes
+        df1.columns = df1.columns.str.lower()
+        df2.columns = df2.columns.str.lower()
+        key_column = key_column.lower()
         result_df = remove_rows_and_save(df2, df1, [key_column])
+        st.write('Shape of new sheet', result_df.shape)
 
         # Convert DataFrame to CSV for download
         csv = result_df.to_csv(index=False)
